b84174c97df975d2ec224189e7cbbfbc5a8b7dfc
Martin Fournier <martin@mfournier.com>
2022-01-31 06:30:04 -0500

    feat: initialize project using Create React App
    Initial repository setup using a create-react-app template. Ejected the
    app and default template files.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: { 'prettier/prettier': ['warn', prettierOptions] },
+    },
+  ],
+};
