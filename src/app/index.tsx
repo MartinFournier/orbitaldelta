8a1bbbfea8bbb0f1cfe9faa16221ed2880ca17c5
Martin Fournier <martin@mfournier.com>
2022-02-05 08:22:25 -0500

    chore: clean up repository

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,3 @@
-/**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
@@ -13,7 +5,7 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from 'styles/global-styles';
 
 import { HomePage } from './pages/HomePage/Loadable';
-import { NotFoundPage } from './components/NotFoundPage/Loadable';
+import { NotFoundPage } from './pages/NotFoundPage/Loadable';
 import { useTranslation } from 'react-i18next';
 import { ErrorHandler } from './components/ErrorHandler';
 import { Engine } from './components/Engine';
