a5f983928d8b18707fbef9813575840323eacfc9
Martin Fournier <martin@mfournier.com>
2022-02-15 21:42:13 -0500

    feat(about): add information to about page

--- src/app/pages/AboutPage/index.tsx
+++ src/app/pages/AboutPage/index.tsx
@@ -1,13 +1,86 @@
-import { Box } from '@mui/material';
+import {
+  Box,
+  Divider,
+  Link,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Typography,
+} from '@mui/material';
 import { Page, AppPageProps } from 'app/pages/Page';
 import * as React from 'react';
+import styled from 'styled-components';
 
 export function AboutPage(props: AppPageProps) {
   return (
     <Page {...props}>
-      <Box>
-        <Box sx={{ my: 4 }}>Lorem Ipsum</Box>
+      <Box sx={{ py: 2 }}>
+        <Typography variant="h2" gutterBottom>
+          About the Game
+        </Typography>
+        <Info>
+          {/* <Typography variant="h5" gutterBottom>
+            abc
+          </Typography> */}
+          <Typography variant="body1">
+            Orbital Δ is a game about automating rocket launches, completing
+            contracts and discovering the universe.
+            <br />
+          </Typography>
+        </Info>
+        <Info>
+          <Typography variant="h4" gutterBottom>
+            Inspiration
+          </Typography>
+          <Typography variant="body1">
+            This game has been heavily inspired by{' '}
+            <Link
+              href="https://store.steampowered.com/app/220200/Kerbal_Space_Program/"
+              rel="nofollow"
+              target="_blank"
+            >
+              Kerbal Space Program
+            </Link>{' '}
+            & by{' '}
+            <Link
+              href="https://github.com/danielyxie/bitburner"
+              rel="nofollow"
+              target="_blank"
+            >
+              Bitburner
+            </Link>
+            .
+          </Typography>
+          <Divider sx={{ my: 2 }} />
+          <Typography variant="h5" gutterBottom>
+            Third Party Credit
+          </Typography>
+          <Typography variant="body1">Lorem Ipsum</Typography>
+          <Divider sx={{ my: 2 }} />
+          <Typography variant="h6" gutterBottom>
+            Tech
+          </Typography>
+          <Typography variant="body2">
+            The game is built and is using:
+          </Typography>
+          <List dense>
+            <ListItem>
+              <ListItemText>React.js</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText>Typescript</ListItemText>
+            </ListItem>
+          </List>
+        </Info>
       </Box>
     </Page>
   );
 }
+
+const Info = styled(Paper)`
+  margin-bottom: ${props => props.theme.spacing(2)};
+  padding: ${props => props.theme.spacing(2)};
+  background-color: ${props => props.theme.palette.bg.light};
+  max-width: 600px;
+`;
